5226b8d6512bcd6b4b0a7c06eb2a2fd14c0c813e
grbrr <damianbecmer@gmail.com>
2022-08-15 17:22:46 +0200

    files put in order, added boot/hid_gamepad/main

--- README.md
+++ README.md
@@ -2,16 +2,17 @@
 
 REPO UNDER CONSTRUCTION
 
-This repository allows you to build a custom Kerbal Space Program controller based on the STAR 266 dashboard and Raspberry Pi Pico. You can modify it to your own design, though I recommend using similar hardware.
+This repository allows you to build a custom Kerbal Space Program controller based on the STAR 266 dashboard and Raspberry Pi Pico. You can modify it to your own design, though I recommend using similar hardware. Tested only with Windows 10 machine.
 
 ## RasPi Pico setup
 
 CircuitPython 7.3.2 with the adafruit_hid library is used to simulate the controller.
 1. Push the BOOTSEL button on RasPi Pico and then plug the USB cable.
-2. Copy the .uf2 file to the RPI-RP2 disk to install CircuitPython.
-3. Copy whole 'adafruit_hid' folder to the CIRCUITPY to add necessary libraries.
+2. Copy the .uf2 file from 'firmware' to the RPI-RP2 disk to install CircuitPython.
+3. Copy whole 'lib' folder to the CIRCUITPY to add necessary libraries.
+4. 
 
 ## Communicating with the board
 
 I am using Visual Studio Code. To set up the IDE properly - install Python and then the three VS Code extensions: Python, Pico-Go and CircuitPython. Remember to choose proper board in the lower right corner. All necessary files should be downloaded by VS Code as a bundle.
-At the end connect one button to GP2 or GP3 and upload config_test.py to the board. If you can scroll the screen with this button (up or down) - you have properly configured the IDE and the board.
+If you want to check the installation - simply connect button (tactswitch or sth) to GP2 and some potentiometer to GP26_A0 and check the 'Game Controller Settings'. See if you can make actions.
---
+++ main.py
@@ -0,0 +1,60 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# You must add a gamepad HID device inside your boot.py file
+# in order to use this example.
+# See this Learn Guide for details:
+# https://learn.adafruit.com/customizing-usb-devices-in-circuitpython/hid-devices#custom-hid-devices-3096614-9
+
+import board
+import digitalio
+import analogio
+import usb_hid
+
+from hid_gamepad import Gamepad
+
+gp = Gamepad(usb_hid.devices)
+
+# Create some buttons. The physical buttons are connected
+# to ground on one side and these and these pins on the other.
+button_pins = (board.GP2, board.GP3, board.GP4, board.GP5)
+
+# Map the buttons to button numbers on the Gamepad.
+# gamepad_buttons[i] will send that button number when buttons[i]
+# is pushed.
+gamepad_buttons = (1, 2, 8, 15)
+
+buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
+for button in buttons:
+    button.direction = digitalio.Direction.INPUT
+    button.pull = digitalio.Pull.UP
+
+# Connect an analog two-axis joystick to A4 and A5.
+ax = analogio.AnalogIn(board.GP26_A0)
+ay = analogio.AnalogIn(board.GP27_A1)
+az = analogio.AnalogIn(board.GP28_A2)
+
+# Equivalent of Arduino's map() function.
+def range_map(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+
+while True:
+    # Buttons are grounded when pressed (.value = False).
+    for i, button in enumerate(buttons):
+        gamepad_button_num = gamepad_buttons[i]
+        if button.value:
+            gp.release_buttons(gamepad_button_num)
+            print(" release", gamepad_button_num, end="")
+        else:
+            gp.press_buttons(gamepad_button_num)
+            print(" press", gamepad_button_num, end="")
+
+    # Convert range[0, 65535] to -127 to 127
+    gp.move_joysticks(
+        x=range_map(ax.value, 0, 65535, -127, 127),
+        y=range_map(ay.value, 0, 65535, -127, 127),
+        z=range_map(az.value, 0, 65535, -127, 127),
+        r_z=range_map(az.value, 0, 65535, -127, 127)
+    )
+    print(" x", ax.value, "y", ay.value, "z", az.value)
